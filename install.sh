e44621f180b21066d72729d0d7d61c3593f72a45
Amir Eshel <amir@eshel.com>
2015-06-16 19:32:12 +0300

    Fixed vundle clone path

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo "Installing vim stuff"
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 mkdir ~/.vim
-git clone https://github.com/gmarik/Vundle.vim.git ./.vim/vundle
+git clone https://github.com/gmarik/Vundle.vim.git ./.vim/bundle/Vundle.vim.git
 vim +PluginInstall +qall
 
 echo "done"
