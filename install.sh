428718d8a4e0db384b4ad22c3f0aaa6001cb3ab2
Amir Eshel <amir@eshel.com>
2017-05-11 15:26:15 +0300

    Migrated to zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 # Installation script for creating the symbolic links from the repository (under ~/.bash/.bash*) to the expected locations of the bash files (~/.bash*)
 
-rm -f ~/.bashrc ~/.gitconfig ~/.gitignore_global ~/.vimrc
+rm -f ~/.zshrc ~/.gitconfig ~/.gitignore_global ~/.vimrc
 
-ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 
@@ -20,4 +20,3 @@ vim +PluginInstall +qall
 brew install git bash-completion
 
 echo "done"
-
